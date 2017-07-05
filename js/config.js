1ed5e9d2bbea74b41cb1c249adbc345a9dc85384
Marc Koster <marckoster89@gmail.com>
2017-07-05 20:11:46 +0200

    init localot

---
+++ js/config.js
@@ -0,0 +1,40 @@
+/**
+ * INSPINIA - Responsive Admin Theme
+ *
+ * Inspinia theme use AngularUI Router to manage routing and views
+ * Each view are defined as state.
+ * Initial there are written state for all view in theme.
+ *
+ */
+function config($stateProvider, $urlRouterProvider, $ocLazyLoadProvider) {
+    //$urlRouterProvider.otherwise("/index/registration");
+
+    $ocLazyLoadProvider.config({
+        // Set to true if you want to see what and when is dynamically loaded
+        debug: false
+    });
+
+    $stateProvider
+
+        .state('index', {
+            abstract: true,
+            url: "/index",
+            templateUrl: "views/common/content.html",
+        })
+        .state('index.registration', {
+            url: "/registration",
+            templateUrl: "views/registration.html",
+            data: { pageTitle: 'Registration Locato' }
+        })
+        .state('index.overview', {
+            url: "/overview",
+            templateUrl: "views/overview.html",
+            data: { pageTitle: 'Übersicht' }
+        })
+}
+angular
+    .module('inspinia')
+    .config(config)
+    .run(function($rootScope, $state) {
+        $rootScope.$state = $state;
+    });
---
+++ js/directives.js
@@ -0,0 +1,35 @@
+/**
+ * INSPINIA - Responsive Admin Theme
+ *
+ */
+
+
+/**
+ * pageTitle - Directive for set Page title - mata title
+ */
+function pageTitle($rootScope, $timeout) {
+    return {
+        link: function(scope, element) {
+            var listener = function(event, toState, toParams, fromState, fromParams) {
+                // Default title - load on Dashboard 1
+                var title = 'INSPINIA | Responsive Admin Theme';
+                // Create your own title pattern
+                if (toState.data && toState.data.pageTitle) title = 'INSPINIA | ' + toState.data.pageTitle;
+                $timeout(function() {
+                    element.text(title);
+                });
+            };
+            $rootScope.$on('$stateChangeStart', listener);
+        }
+    }
+}
+
+
+
+/**
+ *
+ * Pass all functions into module
+ */
+angular
+    .module('inspinia')
+    .directive('pageTitle', pageTitle)
